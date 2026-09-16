6a7176484a90619bdf5756769dcb7ab8b6e67e30
pedro.araujo <pedro.araujo3@icloud.com>
2026-09-16 18:57:43 -0300

    corrigindo o scanner da tabuada

--- src/Atividade4/MainTabuada.java
+++ src/Atividade4/MainTabuada.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class MainTabuada {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-    int numeroTabuada;
-    int resultado;
+        int numeroTabuada;
+        int resultado;
 
-    System.out.println("Bem vindo a Tabuada");
-    System.out.println("\nPara comerçarmos, digite o número para receber a Tabuada dele ate o 10");
-    numeroTabuada = input.nextInt();
+        System.out.println("Bem vindo a Tabuada");
+        System.out.println("\nPara comerçarmos, digite o número para receber a Tabuada dele ate o 10");
+        numeroTabuada = input.nextInt();
+        input.nextLine();
 
-    for(int i = 0; i <= 10; i++) {
-        resultado = numeroTabuada * i;
-        System.out.printf("%d X %d: %d\n", numeroTabuada, i, resultado );
-    }
+
+        for (int i = 0; i <= 10; i++) {
+            resultado = numeroTabuada * i;
+            System.out.printf("%d X %d: %d\n", numeroTabuada, i, resultado);
+        }
     }
 }
